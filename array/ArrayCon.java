b2993e8ecadd93ef3b871364fadc0dd5729b6111
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2024-11-23 22:14:17 +0530

    Create ArrayCon.java

---
+++ array/ArrayCon.java
@@ -0,0 +1,27 @@
+/* problem statement - take array of 5 integer elements and array2 of 3 int elements then 
+  concatenate both array into arr3*/
+
+
+ class ArrayCon {
+
+	public static void main(String[] args) {
+		
+		int arr1[] = {10,20,30,40,50};
+		int arr2[] = {70,80,90};
+		int arr3[] =new int[arr1.length+arr2.length];
+		
+		for(int i =0;i<arr1.length;i++) {
+			arr3[i] = arr1[i];
+		}
+		for(int i =0;i<arr2.length;i++) {
+			arr3[arr1.length+i] = arr2[i];
+			
+		}
+		for(int i =0;i<arr3.length;i++) {
+			System.out.print(arr3[i]+" ");
+			
+		}
+
+	}
+
+}
