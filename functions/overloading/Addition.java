029cb77a1dbfc0cb50dc4dce78eed8700788566c
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2024-12-28 12:24:19 +0530

    Create Addition.java

---
+++ functions/overloading/Addition.java
@@ -0,0 +1,27 @@
+package com.acc.overloading;
+
+public class Addition {
+	
+	int a,b,c,d,e;
+	
+	public Addition() {
+		a=10;
+		b=20;
+	}
+	public void showData() {
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+	public void add() {
+		c=a+b;
+		System.out.println("Sum of instace variable="+c);
+	}
+	public void add(int c) {
+		d=a+b+c;
+		System.out.println("Sum of local variable="+d);
+	}
+	public void add(int c,int d) {
+		e=a+b+c+d;
+		System.out.println("Sum of local variable="+e);
+	}
+}
