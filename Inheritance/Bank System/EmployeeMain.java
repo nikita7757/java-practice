d2c7aea9b4d0e1dac52f9628adbfd291f8546acf
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2025-01-07 20:19:27 +0530

    Create EmployeeMain.java

---
+++ Inheritance/Bank System/EmployeeMain.java
@@ -0,0 +1,14 @@
+package com.acc.Banksystem;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		Employee obj = new Employee();
+		
+		obj.setData("nikita", "mohan nagar", "pune", 20, 'f', 824670, "katraj");
+		obj.setData("cashier", "cashier","nikita","nikita346", 87570, 4, 2, 450);
+		obj.showData();
+		
+	}
+
+}
