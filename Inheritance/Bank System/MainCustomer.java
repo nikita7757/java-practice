24bb2921935fcf1f6bb0014989dc9534a77d70cd
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2025-01-08 20:42:55 +0530

    Create MainCustomer.java

---
+++ Inheritance/Bank System/MainCustomer.java
@@ -0,0 +1,17 @@
+package com.acc.Banksystem;
+
+public class MainCustomer {
+
+	public static void main(String[] args) {
+
+		Customer obj = new Customer();
+		
+		obj.setData("neha", "pune mohan nagar","pune", 30, 'f', 45870, "katraj");
+		obj.setData(4572, 045724, 452480, 50, 805720);
+		obj.showData();
+		
+		
+
+	}
+
+}
