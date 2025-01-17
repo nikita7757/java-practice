033fd947e965a10535442133598465c56c6ca596
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2025-01-17 19:08:14 +0530

    Create ArrayList1.java

---
+++ Collection/Arraylist/ArrayList1.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<String> obj = new ArrayList();
+		
+		obj.add("nisha");
+		obj.add("neha");
+		obj.add("nikita");
+		
+		System.out.println(obj);
+		
+		obj.add(0,"supriya");
+		System.out.println(obj);
+		
+		obj.add(0,"niku");
+		System.out.println(obj);
+		
+		obj.add(0,"Deepti");
+		System.out.println(obj);
+		
+		obj.remove("niku");
+		System.out.println(obj);
+		
+		obj.remove(0);
+		System.out.println(obj);
+		
+		Collections.sort(obj);
+		System.out.println(obj);
+		
+		Collections.reverse(obj);
+		System.out.println(obj);
+	}
+
+}
