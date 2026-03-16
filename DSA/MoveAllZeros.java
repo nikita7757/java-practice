98a1321b5bdfb0ad8ba051e3df4f55dd528032f4
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2026-03-17 00:45:33 +0530

    Create MoveAllZeros.java

---
+++ DSA/MoveAllZeros.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+move all zeros from array to end
+ex arr =[1,0,2,0,2,3] ======[1,2,2,3,0,0]
+with taking inputs from user 
+*******************************************************************************/
+import java.util.*;
+class MoveAllZeros {
+  static  void missingNum(int arr[],int n) {
+        
+      int temp ;;
+      int z = 0;
+      for(int i =0; i<n; i++){
+            if(arr[i]!=0){
+                temp = arr[i];
+                arr[i]= arr[z];
+                arr[z] = temp;
+                z++; 
+            }
+        } 
+       
+    }
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        
+        int arr[] = new int[size];
+        for(int i =0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        missingNum(arr,size);
+        for(int i =0; i<size; i++){
+             System.out.println(arr[i]);
+        }
+       
+    }
+  
+}
